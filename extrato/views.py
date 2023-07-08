0be1924421b02488e1eda62820a0ed6dd42d6291
nicomedes <marechalnicom@gmail.com>
2023-07-08 10:06:05 -0300

    aula2 add extrato

---
+++ extrato/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from perfil.models import Conta, Categoria
+
+def novo_valor(request):
+    if request.method == "GET":
+        contas = Conta.objects.all()
+        categorias = Categoria.objects.all() 
+        return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
